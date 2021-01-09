c01b3749a8e3afcb2508735f7abbe251a773a623
YasithSam <yasith.sam7@gmail.com>
2021-01-10 01:12:36 +0530

    Database deployed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "123",
-    DB: "testdb",
+    HOST: "ec2-3-211-149-196.compute-1.amazonaws.com:5432",
+    USER: "nbanaswdxtrwtj",
+    PASSWORD: "4923d14fa96bd976b85da6f9c2a2ad45093a8727bec727afce19f136082966a5",
+    DB: "d4defus8i1ur8f",
     dialect: "postgres",
     pool: {
       max: 5,
